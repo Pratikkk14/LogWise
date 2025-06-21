ce7c9eebcc177afa8baa839940afdf6519c0059c
Pratikkk14 <pratik146971@gmail.com>
2025-06-21 18:41:19 +0530

    updates the backend to more modular structure

--- backend/main.py
+++ backend/main.py
@@ -1,51 +1,14 @@
+from fastapi import FastAPI
 from dotenv import load_dotenv
-load_dotenv()
 
-from fastapi import FastAPI, Query
-from google.cloud import logging_v2
-from datetime import datetime, timedelta, timezone
-import os
+# Load environment variables
+load_dotenv()
 
 app = FastAPI()
 
-client = logging_v2.Client()
+# Include routes
+from routes.logs import router as logs_router
+# from routes.explain import router as explain_router
 
-@app.get("/logs")
-def get_logs(
-    start_minutes_ago: int = Query(60),
-    end_minutes_ago: int = Query(0),
-    severity: str = Query(None)
-):
-    """Fetch Logs from Google Cloud Logging"""
-    now = datetime.now(timezone.utc)
-    start_time = now - timedelta(minutes=start_minutes_ago)
-    end_time = now - timedelta(minutes=end_minutes_ago)
-    
-    filter_parts = [
-    f'timestamp >= "{start_time.strftime("%Y-%m-%dT%H:%M:%SZ")}"',
-    ]
-    
-    if severity:
-        filter_parts.append(f'severity="{severity}"')
-        
-    filter_str = " AND ".join(filter_parts)
-    
-    print(filter_str) # Debugging filter string
-    
-    entries = client.list_entries(
-        filter_=filter_str,
-        order_by=logging_v2.DESCENDING
-        )
-    
-    logs = []
-    for entry in entries:
-        logs.append(
-            {
-                "timestamp": str(entry.timestamp),
-                "severity": entry.severity,
-                "message": entry.payload if isinstance(entry.payload, str) else str(entry.payload),
-                "resource_type": entry.resource.type,
-                "resource_labels": entry.resource.labels
-            }
-        )
-    return {"logs":logs}        
+app.include_router(logs_router, prefix="/logs")
+# app.include_router(explain_router, prefix="/explain")
